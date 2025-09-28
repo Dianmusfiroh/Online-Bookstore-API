76c62a43d872440472e41e39f1fea73b381a216d
dianmusfiroh <dianmusfiroh90@gmail.com>
2025-09-28 20:52:05 +0800

    ad

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ cd /app
 # Jalankan migrasi terlebih dahulu
 echo "Running database migrations..."
 migrate -database "pgx://postgres:xidOShwWcHtxASEEHcptmJTfFsEDZHVX@$PGHOST:$PGPORT/$PGDATABASE?sslmode=disable" -path migrations up
-/migrations/main
+cd /migrations
+./main
+cd ..
 # Jalankan aplikasi utama
 echo "Starting the main application..."
 ./main
